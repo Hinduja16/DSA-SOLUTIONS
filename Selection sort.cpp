7484775e87f625e2362acdf4cc13ffc9fd1246a6
Hinduja Beesabathini <hinduja1635@gmail.com>
2025-12-17 23:04:15 +0530

    Add files via upload

---
+++ Selection sort.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, min, temp;
+    int arr[50];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
